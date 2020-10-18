9d02883c40ea0c6102c3a186af2ea641cab657d2
nadianadine16 <60763439+nadianadine16@users.noreply.github.com>
2020-10-18 21:13:35 +0700

    Update admin_detailguru.php

--- admin/admin_detailguru.php
+++ admin/admin_detailguru.php
@@ -62,7 +62,6 @@
           <i class="fas fa-fw fa-wrench"></i>
           <span>Guru</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Data Guru:</h6>
